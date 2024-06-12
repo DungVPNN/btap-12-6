029b5348a68b4f5ef1018d19e8cdd0881ceca9fd
DungVPNN <dungbgbg12tt@gmail.com>
2024-06-12 08:01:57 +0700

    ss

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Client from './axios-btap/Client'
+import Home from './axios-btap/Home'
+import Find from './components/Find'
+import Sort from './components/Sort'
+// import Read from './components/Read'
+// import Create from './components/Create'
+// import Delete from './components/Delete'
+// import Update from './components/Update'
+
+export default function App() {
+  return (
+    <div>
+      {/* <Read></Read> */}
+      {/* <Create></Create> */}
+      {/* <Delete></Delete> */}
+      {/* <Update></Update> */}
+      {/* <Find></Find> */}
+      {/* <Sort></Sort> */}
+      {/* <Client></Client> */}
+      
+      <Home></Home>
+    </div>
+  )
+}
